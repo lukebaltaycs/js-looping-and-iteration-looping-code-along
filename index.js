11df7ed6ac2046459edf5bed4186c56fa960a6f1
Luke Baltay <luke.baltay@yale.edu>
2022-07-11 10:04:41 -0400

    commit tree 8f34623289c071e058792d206e61b387582169a8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(recievers, phrase) {
+    let array = []
+    for (let i = 0; i < recievers.length; i++ ){
+        array[i] = `Thank you, ${recievers[i]}, for the wonderful ${phrase} gift!`;
+    }
+    return array;
+}
+
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -14,6 +14,7 @@ describe( 'index.js', () => {
   describe( 'writeCards()', () => {
 
     it( 'returns an array of thank you messages for each name provided to the function', () => {
+      // debugger
       expect( writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) )
         .to.deep.eq( [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ] );
     } );
